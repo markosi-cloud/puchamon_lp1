bdb1776283dfb01d9db83b5176a56c7d28f86629
marco <markosi-cloud>
2026-02-16 18:14:16 -0300

    AÑADIDO PARTE DE JONAS, PROBLEMAS CON RAMA ANTERIOR

--- newGameIntro/script.js
+++ newGameIntro/script.js
@@ -9,15 +9,19 @@ let i = 0;
 let etapa = "dialogo";
 let nombre = "";
 let genero = "";
+let nombreRival = "";
 
 const texto = document.getElementById("text");
 const oak = document.getElementById("oak-sprite");
+const rival = document.getElementById("rival-sprite");
+
+rival.style.display = "none";
 
 texto.innerText = dialogos[i];
 
 document.addEventListener("keydown", (e) => {
 
-    // DIÁLOGOS
+    //  Los dialogos
     if (etapa === "dialogo" && (e.key === "Enter" || e.key === " ")) {
         i++;
         if (i < dialogos.length) {
@@ -28,24 +32,29 @@ document.addEventListener("keydown", (e) => {
         }
     }
 
-    // GÉNERO
+    // Genero del jugador 
     else if (etapa === "genero") {
         if (e.key.toLowerCase() === "h" || e.key.toLowerCase() === "m") {
             genero = e.key.toLowerCase() === "h" ? "Hombre" : "Mujer";
-            etapa = "nombre";
+            etapa = "nombreJugador";
             nombre = "";
             texto.innerText = "Escribe tu nombre:\n";
         }
     }
 
-    // NOMBRE
-    else if (etapa === "nombre") {
+    // El nombre del jugador
+    else if (etapa === "nombreJugador") {
 
         if (e.key === "Enter" && nombre.trim() !== "") {
+
+            // aparece rival
+            rival.style.display = "block";
+           // oak.style.left = "35%";
+          // rival.style.left = "65%";
+
+            etapa = "introRival";
             texto.innerText =
-                nombre + "...\n\nTu aventura estará por comenzar.";
-            oak.style.display = "none";
-            etapa = "fin";
+                nombre + "...\n\nEste es mi nieto...\nSiempre fue tu rival.\n\n¿Cómo se llamaba?";
             return;
         }
 
@@ -57,6 +66,28 @@ document.addEventListener("keydown", (e) => {
         }
 
         texto.innerText = "Escribe tu nombre:\n" + nombre;
+    }
+
+    // El nombre del rival
+    else if (etapa === "introRival") {
+
+        if (e.key === "Enter" && nombreRival.trim() !== "") {
+            texto.innerText =
+                nombre + " y " + nombreRival +
+                "...\n\n¡Su aventura está por comenzar!";
+            etapa = "fin";
+            return;
+        }
+
+        if (e.key === "Backspace") {
+            nombreRival = nombreRival.slice(0, -1);
+        }
+        else if (/^[a-zA-Z0-9]$/.test(e.key) && nombreRival.length < 12) {
+            nombreRival += e.key;
+        }
+
+        texto.innerText =
+            "¿Cómo se llamaba?\n" + nombreRival;
     }
 
 });
